b62d40410fd2880e272211163057ab826561b020
manfredjoa <joamanfred@gmail.com>
2023-08-20 13:48:04 -0400

    set up onMouseEnter and onMouseLeave functions to show respective particles background

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,11 +2,71 @@ import ParticlesWinter from "../components/ParticlesWinter";
 import ParticlesSpring from "../components/ParticlesSpring";
 import ParticlesSummer from "../components/ParticlesSummer";
 import ParticlesAutumn from "../components/ParticlesAutumn";
+import { useState } from "react";
 
 function HomePage() {
+  const [showWinterParticles, setShowWinterParticles] = useState(false);
+  const [showSpringParticles, setShowSpringParticles] = useState(false);
+  const [showSummerParticles, setShowSummerParticles] = useState(false);
+  const [showAutumnParticles, setShowAutumnParticles] = useState(false);
+
+  const showWinter = () => {
+    setShowWinterParticles(true);
+  };
+
+  const showSpring = () => {
+    setShowSpringParticles(true);
+  };
+
+  const showSummer = () => {
+    setShowSummerParticles(true);
+  };
+
+  const showAutumn = () => {
+    setShowAutumnParticles(true);
+  };
+
+  const noShow = () => {
+    setShowWinterParticles(false);
+    setShowSpringParticles(false);
+    setShowSummerParticles(false);
+    setShowAutumnParticles(false);
+  };
+
   return (
     <div>
-      <h1 className="text-center text-5xl text-white">HomePage</h1>
+      <h1
+        className="text-center text-5xl text-white"
+        onMouseEnter={showWinter}
+        onMouseLeave={noShow}
+      >
+        Winter
+      </h1>
+      <h1
+        className="text-center text-5xl text-white"
+        onMouseEnter={showSpring}
+        onMouseLeave={noShow}
+      >
+        Spring
+      </h1>
+      <h1
+        className="text-center text-5xl text-white"
+        onMouseEnter={showSummer}
+        onMouseLeave={noShow}
+      >
+        Summer
+      </h1>
+      <h1
+        className="text-center text-5xl text-white"
+        onMouseEnter={showAutumn}
+        onMouseLeave={noShow}
+      >
+        Fall
+      </h1>
+      {(showWinterParticles && <ParticlesWinter />) ||
+        (showSpringParticles && <ParticlesSpring />) ||
+        (showSummerParticles && <ParticlesSummer />) ||
+        (showAutumnParticles && <ParticlesAutumn />)}
     </div>
   );
 }
