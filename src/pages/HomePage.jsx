7f0b70b758ea65bc57b0c285415578be0079638a
manfredjoa <joamanfred@gmail.com>
2023-08-20 17:41:08 -0400

    create flex box layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -26,6 +26,8 @@ function HomePage() {
     setShowAutumnParticles(true);
   };
 
+  // Can probably add this to beginning of each show function and eliminate onMouseLeave functions
+  // This is if I want the particles to persist on the screen after the mouse leaves the div
   const noShow = () => {
     setShowWinterParticles(false);
     setShowSpringParticles(false);
@@ -34,51 +36,56 @@ function HomePage() {
   };
 
   return (
-    <div className="height-screen bg-black">
-      <div
-        className="text-center text-5xl text-white 
-        "
-        // bg-gradient-to-b from-sky-950 to-white
-        style={{ height: "25vh" }}
-        onMouseEnter={showWinter}
-        onMouseLeave={noShow}
-      >
-        Winter
+    <div className="w-screen bg-black" style={{ height: "100vh" }}>
+      <div className="flex flex-wrap h-screen w-2/4 m-auto">
+        <div className="flex items-end">
+          <div
+            className="text-5xl text-white flex justify-center items-center"
+            // bg-gradient-to-b from-sky-950 to-white
+            style={{ height: "25vw", width: "25vw" }}
+            onMouseEnter={showAutumn}
+            onMouseLeave={noShow}
+          >
+            About
+          </div>
+
+          <div
+            className="text-5xl text-white flex justify-center items-center"
+            // bg-gradient-to-b from-sky-400 via-rose-200 to-green-300
+            style={{ height: "25vw", width: "25vw" }}
+            onMouseEnter={showSpring}
+            onMouseLeave={noShow}
+          >
+            Projects
+          </div>
+        </div>
+
+        <div className="flex items-start">
+          <div
+            className="text-5xl text-white flex justify-center items-center"
+            // bg-gradient-to-b from-amber-300 via-sky-400 to-yellow-700
+            style={{ height: "25vw", width: "25vw" }}
+            onMouseEnter={showWinter}
+            onMouseLeave={noShow}
+          >
+            Resume
+          </div>
+          <div
+            className="text-5xl text-white flex justify-center items-center"
+            // bg-gradient-to-b from-sky-600 to-orange-800
+            style={{ height: "25vw", width: "25vw" }}
+            onMouseEnter={showSummer}
+            onMouseLeave={noShow}
+          >
+            Contact
+          </div>
+        </div>
+
+        {(showWinterParticles && <ParticlesWinter />) ||
+          (showSpringParticles && <ParticlesSpring />) ||
+          (showSummerParticles && <ParticlesSummer />) ||
+          (showAutumnParticles && <ParticlesAutumn />)}
       </div>
-      <div
-        className="height-1/4 text-center text-5xl text-white 
-        "
-        // bg-gradient-to-b from-sky-400 via-rose-200 to-green-300
-        style={{ height: "25vh" }}
-        onMouseEnter={showSpring}
-        onMouseLeave={noShow}
-      >
-        Spring
-      </div>
-      <div
-        className="height-1/4 text-center text-5xl text-white 
-        "
-        // bg-gradient-to-b from-amber-300 via-sky-400 to-yellow-700
-        style={{ height: "25vh" }}
-        onMouseEnter={showSummer}
-        onMouseLeave={noShow}
-      >
-        Summer
-      </div>
-      <div
-        className="height-1/4 text-center text-5xl text-white 
-        "
-        // bg-gradient-to-b from-sky-600 to-orange-800
-        style={{ height: "25vh" }}
-        onMouseEnter={showAutumn}
-        onMouseLeave={noShow}
-      >
-        Fall
-      </div>
-      {(showWinterParticles && <ParticlesWinter />) ||
-        (showSpringParticles && <ParticlesSpring />) ||
-        (showSummerParticles && <ParticlesSummer />) ||
-        (showAutumnParticles && <ParticlesAutumn />)}
     </div>
   );
 }
