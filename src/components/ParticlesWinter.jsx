faa097357703f7dced29c733c0002bf9c65ab48b
manfredjoa <joamanfred@gmail.com>
2023-08-27 18:27:46 -0400

    update winter particles images to cloudinary links

--- src/components/ParticlesWinter.jsx
+++ src/components/ParticlesWinter.jsx
@@ -45,7 +45,7 @@ function ParticlesWinter() {
           options: {
             image: [
               {
-                src: "https://www.freepnglogos.com/uploads/snowflake-png/snowflake-png-eden-ice-cider-vermont-ice-cider-32.png",
+                src: "https://res.cloudinary.com/doqgufzuq/image/upload/v1693175175/Portfolio/Winter/snowflake-png-eden-ice-cider-vermont-ice-cider-32_dkhpgx.png",
                 particles: {
                   size: {
                     value: { min: 4, max: 12 },
@@ -53,7 +53,7 @@ function ParticlesWinter() {
                 },
               },
               {
-                src: "https://pngfre.com/wp-content/uploads/snowflake-png-from-pngfre-2-1-300x300.png",
+                src: "https://res.cloudinary.com/doqgufzuq/image/upload/v1693175152/Portfolio/Winter/snowflake-png-from-pngfre-2-1-300x300_konfvv.png",
                 particles: {
                   size: {
                     value: { min: 4, max: 12 },
@@ -61,7 +61,7 @@ function ParticlesWinter() {
                 },
               },
               {
-                src: "https://pngfre.com/wp-content/uploads/snowflake-png-from-pngfre-32-1-287x300.png",
+                src: "https://res.cloudinary.com/doqgufzuq/image/upload/v1693175126/Portfolio/Winter/snowflake-png-from-pngfre-32-1-287x300_mjrg3t.png",
                 particles: {
                   size: {
                     value: { min: 4, max: 12 },
@@ -69,7 +69,7 @@ function ParticlesWinter() {
                 },
               },
               {
-                src: "https://pngfre.com/wp-content/uploads/snowflake-png-from-pngfre-39-300x300.png",
+                src: "https://res.cloudinary.com/doqgufzuq/image/upload/v1693175100/Portfolio/Winter/snowflake-png-from-pngfre-39-300x300_t8cunu.png",
                 particles: {
                   size: {
                     value: { min: 4, max: 12 },
@@ -77,7 +77,7 @@ function ParticlesWinter() {
                 },
               },
               {
-                src: "https://pngfre.com/wp-content/uploads/snowflake-png-from-pngfre-33-300x300.png",
+                src: "https://res.cloudinary.com/doqgufzuq/image/upload/v1693175044/Portfolio/Winter/snowflake-png-from-pngfre-33-300x300_d7hbfm.png",
                 particles: {
                   size: {
                     value: { min: 4, max: 12 },
