19f5a561e611c804337dc78e391267dfed8df5e3
manfredjoa <joamanfred@gmail.com>
2023-09-07 16:21:56 -0400

    add name

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,71 +14,80 @@ function Home({
   showAutumn,
 }) {
   return (
-    <div className="flex items-center h-screen w-screen">
-      <div className="flex flex-wrap w-2/4 mx-auto">
-        <div className="flex justify-center items-end w-screen mb-5">
-          <div
-            className="text-5xl text-gray-200 flex justify-center items-center z-10 mr-5 border-white border-2 hover:opacity-70 transition duration-500 ease-in-out"
-            style={{
-              height: "15vw",
-              width: "15vw",
-              backgroundImage:
-                "url(https://res.cloudinary.com/doqgufzuq/image/upload/v1694114483/Portfolio/Photos/Autumn.jpg",
-              backgroundSize: "cover",
-            }}
-            onMouseEnter={showAutumn}
-          >
-            About
+    <div>
+      <div
+        className="flex items-center justify-center z-10 text-white w-screen absolute"
+        style={{ height: "12.5vh" }}
+      >
+        <h1 className="text-center text-6xl">Manfred Joa</h1>
+      </div>
+
+      <div className="flex items-center h-screen w-screen">
+        <div className="flex flex-col h-3/4 w-2/4 items-center justify-center m-auto">
+          <div className="flex justify-center items-end w-screen mb-5">
+            <div
+              className="text-5xl text-gray-200 flex justify-center items-center z-10 mr-5 border-white border-2 hover:opacity-70 transition duration-500 ease-in-out"
+              style={{
+                height: "15vw",
+                width: "15vw",
+                backgroundImage:
+                  "url(https://res.cloudinary.com/doqgufzuq/image/upload/v1694114483/Portfolio/Photos/Autumn.jpg",
+                backgroundSize: "cover",
+              }}
+              onMouseEnter={showAutumn}
+            >
+              About
+            </div>
+
+            <div
+              className="text-5xl text-gray-200 flex justify-center items-center z-10 ml-5 border-white border-2 hover:opacity-70 transition duration-500 ease-in-out"
+              style={{
+                height: "15vw",
+                width: "15vw",
+                background:
+                  "url(https://res.cloudinary.com/doqgufzuq/image/upload/v1694114483/Portfolio/Photos/Spring.jpg)",
+                backgroundSize: "cover",
+              }}
+              onMouseEnter={showSpring}
+            >
+              Projects
+            </div>
           </div>
 
-          <div
-            className="text-5xl text-gray-200 flex justify-center items-center z-10 ml-5 border-white border-2 hover:opacity-70 transition duration-500 ease-in-out"
-            style={{
-              height: "15vw",
-              width: "15vw",
-              background:
-                "url(https://res.cloudinary.com/doqgufzuq/image/upload/v1694114483/Portfolio/Photos/Spring.jpg)",
-              backgroundSize: "cover",
-            }}
-            onMouseEnter={showSpring}
-          >
-            Projects
+          <div className="flex justify-center items-start w-screen mt-5">
+            <div
+              className="text-5xl text-gray-200 flex justify-center items-center z-10 mr-5 border-white border-2 hover:opacity-70 transition duration-500 ease-in-out"
+              style={{
+                height: "15vw",
+                width: "15vw",
+                background:
+                  "url(https://res.cloudinary.com/doqgufzuq/image/upload/v1694114483/Portfolio/Photos/Winter.jpg)",
+                backgroundSize: "cover",
+              }}
+              onMouseEnter={showWinter}
+            >
+              Resume
+            </div>
+            <div
+              className="text-5xl text-gray-200 flex justify-center items-center z-10 ml-5 border-white border-2 hover:opacity-70 transition duration-500 ease-in-out"
+              style={{
+                height: "15vw",
+                width: "15vw",
+                background:
+                  "url(https://res.cloudinary.com/doqgufzuq/image/upload/v1694114483/Portfolio/Photos/Summer.jpg)",
+                backgroundSize: "cover",
+              }}
+              onMouseEnter={showSummer}
+            >
+              Contact
+            </div>
           </div>
+
+          {(showWinterParticles && <ParticlesWinter />) ||
+            (showSpringParticles && <ParticlesSpring />) ||
+            (showSummerParticles && <ParticlesSummer />) ||
+            (showAutumnParticles && <ParticlesAutumn />)}
         </div>
-
-        <div className="flex justify-center items-start w-screen mt-5">
-          <div
-            className="text-5xl text-gray-200 flex justify-center items-center z-10 mr-5 border-white border-2 hover:opacity-70 transition duration-500 ease-in-out"
-            style={{
-              height: "15vw",
-              width: "15vw",
-              background:
-                "url(https://res.cloudinary.com/doqgufzuq/image/upload/v1694114483/Portfolio/Photos/Winter.jpg)",
-              backgroundSize: "cover",
-            }}
-            onMouseEnter={showWinter}
-          >
-            Resume
-          </div>
-          <div
-            className="text-5xl text-gray-200 flex justify-center items-center z-10 ml-5 border-white border-2 hover:opacity-70 transition duration-500 ease-in-out"
-            style={{
-              height: "15vw",
-              width: "15vw",
-              background:
-                "url(https://res.cloudinary.com/doqgufzuq/image/upload/v1694114483/Portfolio/Photos/Summer.jpg)",
-              backgroundSize: "cover",
-            }}
-            onMouseEnter={showSummer}
-          >
-            Contact
-          </div>
-        </div>
-
-        {(showWinterParticles && <ParticlesWinter />) ||
-          (showSpringParticles && <ParticlesSpring />) ||
-          (showSummerParticles && <ParticlesSummer />) ||
-          (showAutumnParticles && <ParticlesAutumn />)}
       </div>
     </div>
   );
