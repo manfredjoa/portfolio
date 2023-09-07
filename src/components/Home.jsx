84da30d42398bab8ad76974e3f7117fd3de6215b
manfredjoa <joamanfred@gmail.com>
2023-09-07 15:36:56 -0400

    add images to Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,19 +18,29 @@ function Home({
       <div className="flex flex-wrap w-2/4 mx-auto">
         <div className="flex justify-center items-end w-screen mb-5">
           <div
-            className="text-5xl text-gray-200 flex justify-center items-center z-10 mr-5 border-white border-2"
-            style={{ height: "15vw", width: "15vw" }}
+            className="text-5xl text-gray-200 flex justify-center items-center z-10 mr-5 border-white border-2 hover:opacity-70 transition duration-500 ease-in-out"
+            style={{
+              height: "15vw",
+              width: "15vw",
+              backgroundImage:
+                "url(https://res.cloudinary.com/doqgufzuq/image/upload/v1694114483/Portfolio/Photos/Autumn.jpg",
+              backgroundSize: "cover",
+            }}
             onMouseEnter={showAutumn}
-            // onMouseLeave={noShow}
           >
             About
           </div>
 
           <div
-            className="text-5xl text-gray-200 flex justify-center items-center z-10 ml-5 border-white border-2"
-            style={{ height: "15vw", width: "15vw" }}
+            className="text-5xl text-gray-200 flex justify-center items-center z-10 ml-5 border-white border-2 hover:opacity-70 transition duration-500 ease-in-out"
+            style={{
+              height: "15vw",
+              width: "15vw",
+              background:
+                "url(https://res.cloudinary.com/doqgufzuq/image/upload/v1694114483/Portfolio/Photos/Spring.jpg)",
+              backgroundSize: "cover",
+            }}
             onMouseEnter={showSpring}
-            // onMouseLeave={noShow}
           >
             Projects
           </div>
@@ -38,18 +48,28 @@ function Home({
 
         <div className="flex justify-center items-start w-screen mt-5">
           <div
-            className="text-5xl text-gray-200 flex justify-center items-center z-10 mr-5 border-white border-2"
-            style={{ height: "15vw", width: "15vw" }}
+            className="text-5xl text-gray-200 flex justify-center items-center z-10 mr-5 border-white border-2 hover:opacity-70 transition duration-500 ease-in-out"
+            style={{
+              height: "15vw",
+              width: "15vw",
+              background:
+                "url(https://res.cloudinary.com/doqgufzuq/image/upload/v1694114483/Portfolio/Photos/Winter.jpg)",
+              backgroundSize: "cover",
+            }}
             onMouseEnter={showWinter}
-            // onMouseLeave={noShow}
           >
             Resume
           </div>
           <div
-            className="text-5xl text-gray-200 flex justify-center items-center z-10 ml-5 border-white border-2"
-            style={{ height: "15vw", width: "15vw" }}
+            className="text-5xl text-gray-200 flex justify-center items-center z-10 ml-5 border-white border-2 hover:opacity-70 transition duration-500 ease-in-out"
+            style={{
+              height: "15vw",
+              width: "15vw",
+              background:
+                "url(https://res.cloudinary.com/doqgufzuq/image/upload/v1694114483/Portfolio/Photos/Summer.jpg)",
+              backgroundSize: "cover",
+            }}
             onMouseEnter={showSummer}
-            // onMouseLeave={noShow}
           >
             Contact
           </div>
