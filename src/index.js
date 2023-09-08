cf0793f78f4a1b67612fabd5128fbfc40e98196b
manfredjoa <joamanfred@gmail.com>
2023-09-08 12:43:32 -0400

    set up material tailwind

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 import App from "./App";
 import "./tailwind.css";
+import { ThemeProvider } from "@material-tailwind/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <App />
+      </Router>
+    </ThemeProvider>
   </React.StrictMode>
 );
